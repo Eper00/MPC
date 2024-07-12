593d4aacfee38f77433c95bfe657018649a8df04
Eper69 <104941550+Eper69@users.noreply.github.com>
2024-07-12 10:34:06 +0200

    Add files via upload

--- optimalization.py
+++ optimalization.py
@@ -3,49 +3,87 @@ import pyomo.environ as pyo
 import pyomo.opt as po
 import matplotlib.pyplot as plt
 import optimalization_modell as m
+population=m.population
+Latent=m.Latent
+dt=m.dt
+t_end=100.
+
+x0 = [population-Latent, Latent,0.,0.,0.,0.,0.,0.]
+x_init=np.zeros((8,int(t_end/dt)))
+u_values={0: 0.,
+          1: 0.1,
+          2: 0.2,
+          3: 0.3,
+          4: 0.4,
+          5: 0.5,
+          6: 0.6,
+          7: 0.7,
+          8: 0.8,
+          9: 0.9}
+
+
 
 def create_model (x0param,xinitparam):
     model=pyo.ConcreteModel()
-    model.horizont=range(50)
+    model.horizont=range(int(t_end/dt))
     model.dim=range(8)
     
     model.u_values = pyo.Set(initialize=u_values.values())
-    model.x=pyo.Var(model.horizont,model.dim,domain=pyo.Reals)
+    model.x=pyo.Var(model.horizont,model.dim,domain=pyo.NonNegativeReals)
     model.u=pyo.Var(model.horizont,domain=pyo.Reals)
     model.u_idx=pyo.Var(model.horizont,range(len(model.u_values)),domain=pyo.Binary)
     
     model.constraints = pyo.ConstraintList()
     model.u_idx_constraints = pyo.ConstraintList()
     model.u_rule_constraints=pyo.ConstraintList()
+    model.hospital_capacity=pyo.ConstraintList()
 
     model.obj = pyo.Objective(rule=lambda model: obj_rule(model), sense=pyo.minimize)
         
     for i in model.horizont:
         for j in model.dim:
             model.x[i,j].value=xinitparam[j,i]
+                       
+    for j in model.dim:
             model.x[0,j].fix(x0param[j])
 
-    u_idx_const(model)
-    u_rule(model)
+
+            
+
+    #u_idx_const(model)
+    #u_rule(model)
+    #hospital_capacity_constraint(model)
     system_dynamic(model)
     return model
+
+
+def hospital_capacity_constraint(model):
+    for t in model.horizont:
+        model.hospital_capacity.add(
+            model.x[t,5]<=10000
+        )
 
 
 def obj_rule(model):
     return sum((model.u[t])**2 for t in model.horizont)
 
 def real_model(model):
+    #Ide a valós rendszer, folytonos jön
     return 0
 
 def system_dynamic(model):
-
-    for t in model.horizont:
-          if t < max(model.horizont):
-            res=m.runge_kutta_4_step(model,t)
-            for j in model.dim:
+    t=0
+    while t<t_end:
+        y0=[None]*len(model.dim)
+        for i in model.dim:
+            y0[i]=model.x[t,i]
+        res=m.runge_kutta_4_step(y0,model.u[t])
+        for j in model.dim:
+            if t<max(model.horizont):
                 model.constraints.add(
-                model.x[t+1,j]==res[j]
-                    )            
+                model.x[t+1,j]==res[j] 
+                )
+        t=t+dt        
     
 def u_rule(model):
     for t in model.horizont:
@@ -61,21 +99,21 @@ def u_idx_const(model):
     for t in model.horizont:
          model.u_idx_constraints.add(sum(model.u_idx[t,:])==1)
 
-population=9800000
-Latent=10
 
-x0 = [population-Latent, Latent,0.,0.,0.,0.,0.,0.]
-x_init=np.zeros((8,50))
-u_values={0: 0.,
-          1: 0.1,
-          2: 0.2,
-          3: 0.3,
-          4: 0.4,
-          5: 0.5,
-          6: 0.6,
-          7: 0.7,
-          8: 0.8,
-          9: 0.9}
+
 
 M=create_model(x0,x_init)
-M.x.display()
+solution=pyo.SolverFactory('baron').solve(M, tee=True)
+
+y_values=[None]*len(M.horizont)
+for i in M.horizont:
+    y_values[i]=M.x[i,0].value
+    
+t_values=np.linspace(0,len(M.horizont),len(M.horizont))
+
+fig = plt.figure(figsize=(10,10))
+plt.plot(t_values, y_values,linestyle="",marker=".")
+    
+plt.grid()    
+plt.show()
+print(y_values[99])
--- optimalization_modell.py
+++ optimalization_modell.py
@@ -1,9 +1,21 @@
 import numpy as np
-def dydt(S, L, P, I, A, H, u):
+population=9800000.
+dt=1.
+Latent=10.
+def dydt(t, y,u=0):
+   
                     #0->beta   #1->delta    #2->N          #3->alpha    #4->p    #5->q     #6->ro_1   #7->ro_a  #8->eta  #9->h    #10->mikro
-    param=np.array([1/3 ,     0.75 ,       9800000,    1/2.5 ,      1/3 ,    0.6 ,      1/4 ,     1/4 ,     0.076 ,  1/10 ,    0.145])
-    dSdt = -param[0]*(1.-u)*(P+I+A*param[1])*S/param[2]
-    dLdt=param[0]*(1.-u)*(P+I+A*param[1])*S/param[2]-param[3]*L
+    param=np.array([1/3 ,     0.75 ,       population,    1/2.5 ,      1/3 ,    0.6 ,      1/4 ,     1/4 ,     0.076 ,  1/10 ,    0.145])
+    S = y[0]
+    L = y[1]
+    P = y[2]
+    I = y[3]
+    A = y[4]
+    H = y[5]
+    R = y[6]
+    D = y[7]
+    dSdt = -param[0]*(1-u)*(P+I+A*param[1])*S/param[2]
+    dLdt=param[0]*(1-u)*(P+I+A*param[1])*S/param[2]-param[3]*L
     dPdt=param[3]*L-param[4]*P
     dIdt=param[4]*param[5]*P-param[6]*I
     dAdt=(1-param[5])*param[4]*P-param[7]*A
@@ -11,19 +23,32 @@ def dydt(S, L, P, I, A, H, u):
     dRdt=param[6]*(1-param[8])*I+param[7]*A+(1-param[10])*param[9]*H
     dDdt=param[10]*param[9]*H
     return [dSdt, dLdt,dPdt,dIdt,dAdt,dHdt,dRdt,dDdt]
-def runge_kutta_4_step(model,t):
 
-    dt=0.5
-    
-   
-    k1 = dydt( model.x[t,0],model.x[t,1],model.x[t,2],model.x[t,3],model.x[t,4],model.x[t,5],model.u[t])
-    k2 = dydt( model.x[t,0]+ dt/2 * k1[0],model.x[t,1]+ dt/2 * k1[1],model.x[t,2]+ dt/2 * k1[2],model.x[t,3]+ dt/2 * k1[3],model.x[t,4]+ dt/2 * k1[4],model.x[t,5]+ dt/2 * k1[5],model.u[t])
-    k3 = dydt( model.x[t,0]+ dt/2 * k2[0],model.x[t,1]+ dt/2 * k2[1],model.x[t,2]+ dt/2 * k2[2],model.x[t,3]+ dt/2 * k2[3],model.x[t,4]+ dt/2 * k2[4],model.x[t,5]+ dt/2 * k2[5],model.u[t])
-    k4 = dydt( model.x[t,0]+ dt * k3[0],model.x[t,1]+ dt * k3[1],model.x[t,2]+ dt * k3[2],model.x[t,3]+ dt * k3[3],model.x[t,4]+ dt * k3[4],model.x[t,5]+ dt * k3[5],model.u[t])
-    
-    
-    res=[]
-    for i in model.dim:
-        res.append(model.x[t,i] + dt/6 * (k1[i] + 2*k2[i] + 2*k3[i] + k4[i]))
+def scalar(scalar,array):
+    res=[None]*len(array)
+    for i in range(len(array)):
+        res[i]=array[i]*scalar
     return res
 
+def summation(first,second,third=None,fourth=None):
+    if third is None:
+        third = [0] * len(first)
+    if fourth is None:
+        fourth = [0] * len(first)
+    S=[None]*len(first)
+    for i in range(len(first)):
+        S[i]=first[i]+second[i]+third[i]+fourth[i]
+
+    return S
+
+def runge_kutta_4_step(y0,u):
+    y = y0
+    u=u
+    t=0
+    k1 = dydt(t, y, u)
+    k2 = dydt(t + dt/2, summation(y , scalar(dt/2 , k1)),u)
+    k3 = dydt(t + dt/2, summation(y , scalar(dt/2 , k2)),u)
+    k4 = dydt(t + dt, summation(y , scalar(dt , k3)),u)
+    K=summation(k1 , scalar(2,k2) , scalar(2 , k3) , k4)
+    y = summation(y, scalar(dt/6,K))
+    return y
