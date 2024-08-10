9b28015ac62eaad41856b6be1fa6186957e91a56
Eper69 <104941550+Eper69@users.noreply.github.com>
2024-08-10 20:51:56 +0200

    Add files via upload

--- wtc_prot_2d.py
+++ wtc_prot_2d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyomo.environ as pyo
+import pyomo.opt as po
 import matplotlib.pyplot as plt
 
 # Ezek azok a bevatkozási értékek amelyekkel beavatkozhatunk (U halmaz).
@@ -21,16 +22,18 @@ tmax=np.ones(len(u_values))*4
 # Függvény amely az optimalizációs problémát hozza létre:
 # Bemenet: A rendszert leíró dinamika, kiinduló állapot, illetve az inicializációs vektor
 # Kimenet: Az optimaliálandó probléma (obcejt és a határtok)
-def model_create(Aparam,Bparam,xreqparam,x0param):
+def model_create(Aparam,Bparam,xreqparam,x0param,xinitparam):
     model = pyo.ConcreteModel() 
     model.horizont=range(30)
+    model.dim=range(2)
     model.A=Aparam
     model.B=Bparam
     model.xreq=xreqparam
     
+    model.x=pyo.Var(model.horizont,model.dim,domain=pyo.Reals)
+    
     model.u_values = pyo.Set(initialize=u_values.values())
     
-    model.x=pyo.Var(model.horizont)
     model.u = pyo.Var(model.horizont,domain=model.u_values)
     model.u_idx=pyo.Var(model.horizont,range(len(model.u_values)),domain=pyo.Binary)
     model.H=pyo.Var(range(len(model.horizont)+1),range(len(model.u_values)),domain=pyo.NonNegativeIntegers)
@@ -45,8 +48,13 @@ def model_create(Aparam,Bparam,xreqparam,x0param):
 
     model.obj = pyo.Objective(rule=lambda model: obj_rule(model), sense=pyo.minimize)
     
-    model.x[0].fix(x0param)
+    for i in model.horizont:
+        for j in model.dim:
+            model.x[i,j].value=xinitparam[j,i]
 
+
+
+    model.x[0,:].fix(x0param[0])
     h_counting(model)
     u_idx_const(model)
     u_rule(model)
@@ -78,13 +86,14 @@ def wtc_rule_downer(model):
                 res1 = res1 + (model.u_idx[t, j] * (tmin[j] - model.H[t+1, j]))
                 res2 = res2 + (model.u_idx[t, j] * model.u_idx[t+1, j]) 
         res2 = res2 * 1200000
-        model.wtc_constraints_downer.add( res1 <= res2)                            
+        model.wtc_constraints_downer.add( res1 <= res2) 
+
 # Függvény, amely biztosítja, hogy a bevatkozás csak a megadott U halmazból kerüljön ki (7-es egyenlet)
 def u_rule(model):
     for t in model.horizont:
         res=0
-        for i in range(len(model.u_values)):
-            res=model.u_idx[t,i]*u_values[i]+res
+        for j in range(len(model.u_values)):
+             res=model.u_idx[t,j]*u_values[j]+res
             
         model.u_rule_constraints.add(model.u[t]==res)
     
@@ -92,71 +101,80 @@ def u_rule(model):
 # Függvény amely biztosístja hogy egy adott időpontban csak egy index legyen aktív (8-as egyenlet)
 def u_idx_const(model):
     for t in model.horizont:
-        model.u_idx_constraints.add(sum(model.u_idx[t,:])==1)    
-# Az optimalizlandó kifejezés (3-as egynelet)
+         model.u_idx_constraints.add(sum(model.u_idx[t,:])==1)
+
+        
+# Az optimalizlandó kifejezés (3-as egynelet
 def obj_rule(model):
-    return sum((model.x[t] - model.xreq)**2 for t in model.horizont)
+    return sum((model.x[t, d] - model.xreq[d])**2 for t in model.horizont for d in model.dim)
 # A rendszer dinamikát megvalósító hatrátok (1-es egynelet)
 def system_dynamic(model):
     for t in model.horizont:
         if t < max(model.horizont):
-            model.constraints.add(model.x[t+1] == model.A * model.x[t] + model.B * model.u[t])
-           
+            model.constraints.add(
+                model.x[t+1, 0] == sum(model.A[0, j] * model.x[t, j] for j in model.dim) + model.B[0] * model.u[t]
+            )
+            model.constraints.add(
+                model.x[t+1, 1] == sum(model.A[1, j] * model.x[t, j] for j in model.dim) + model.B[1] * model.u[t]
+            )
+    
 # A rendszer dinamikát megahatározó paraméterek
-A = 1.1
-B = 1.5
+A = np.array([[1,1.4],[1.4,1]])
+B = np.array([1.5,1.5])
+
 
 # Az állapot amelybe a rendszert irányítani akarjuk
-xreq=np.array([100.])
+xreq=np.array([100.,100.])
 # Kiniduló állapot 
-x0=np.array([0.])
+x0=np.array([0.,0.])
+xinit=np.zeros((2,30),float)
 
 # Az  idő horizont hossza
 total_time=30
 
-# Vektorok melyek az adat kirajzoltatását segítik elő
-total_time_u=np.linspace(0,total_time,total_time)
-total_time_x=np.linspace(0,total_time+1,total_time)
-# Adatárolók melyekbe az eredményt kapjuk
-xsystem = np.ones(30)
-usystem=np.ones(30)
-
+total_time_u=np.linspace(0,total_time,total_time+1)
+total_time_x=np.linspace(0,total_time+1,total_time+2)
 
 # A solver típusának beállítása
-solver = pyo.SolverFactory('gurobi')
-
-
-
+solver = po.SolverFactory('gurobi')
 # A konrét modell "példányosítása"
-model=model_create(A,B,xreq,x0)
-
+model=model_create(A,B,xreq,x0,xinit)
 # A probléma megoldása 
-solution=solver.solve(model)
+results = solver.solve(model)
+# Adatárolók melyekbe az eredményt kapjuk
+xsystem = np.ones((len(model.dim),len(model.horizont)))
+usystem=np.ones(len(model.horizont))
 # A kapott adatok eltárolása
-for i in range(total_time):
+for i in model.horizont:
+    for j in model.dim:
+        xsystem[j,i]=model.x[i,j].value
     usystem[i]=model.u[i].value
-    xsystem[i] = model.x[i].value
 
-
-
+# Vektorok melyek az adat kirajzoltatását segítik elő
+xtime=np.linspace(0,len(model.horizont),len(model.horizont))
+utime=np.linspace(0,np.shape(usystem)[0],np.shape(usystem)[0])
 # És végül az adtok vizualizációja 
-plt.figure(figsize=(12, 6))
-plt.subplot(1,2,1)
-plt.title("Control signal")
-plt.xlabel("Time [s]")
-plt.ylabel("U")
-plt.plot(total_time_u,np.squeeze(usystem),color="b",linestyle="",marker="o")
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(2, 1, 1)
+ax.set_xlabel("Time [s]")
+ax.set_ylabel("Control Signal")
 plt.grid()
 y_ticks = np.arange(np.min(usystem), np.max(usystem) + 1, 1)
 plt.yticks(y_ticks)
+x_ticks = np.arange(0, len(utime) + 1, 1)
+plt.xticks(x_ticks)
+ax.plot(utime,usystem,linestyle="",marker="o")
+ax = fig.add_subplot(2, 1, 2,projection='3d')
+
+ax.plot3D(xsystem[0,:], xsystem[1,:],xtime, 'green')
+ax.set_zlabel("Time [s]")
+ax.set_xlabel("x1 State")
+ax.set_ylabel("x2 state")
 
 
-plt.subplot(1,2,2)
-plt.title("State of the system")
-plt.xlabel("Time [s]")
-plt.ylabel("X")
-plt.plot(total_time_x,np.squeeze(xsystem),color="r",linestyle="-",marker="o")
-plt.grid()
-y_ticks = np.arange(np.min(xsystem), np.max(xsystem) + 1, 10)
-plt.yticks(y_ticks)
+
+
+
 plt.show()
+
+
